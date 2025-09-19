45af852b0133ec7863249aebc74e94d4bd9b23fa
lalityenni <lalit.yenni@gmail.com>
2025-09-18 18:56:23 -0700

    ch02: add management group resource

--- infra/ch02-rbac/main.tf
+++ infra/ch02-rbac/main.tf
@@ -12,9 +12,17 @@ terraform {
     
   } 
 
-
+# Create a management group
   resource "azurerm_management_group" "mg1" {
     display_name = var.mg_display_name
     name         = var.mg_name
 
   }
+
+# Task 2: Assign VM Contributor role to Helpdesk group at MG scope
+
+resource "azurerm_role_assignment" "helpdesk_vm_contributor" {
+    scope = azurerm_management_group.mg1.id
+    role_definition_name = "Virtual Machine Contributor"
+    principal_id = var.helpdesk_object_id
+}
